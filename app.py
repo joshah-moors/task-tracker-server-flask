c575a438e4f538b5af4298baa85f4acb406f43b5
Joshah <joshah.moors@utexas.edu>
2020-03-08 10:17:22 -0500

    Added tasks endpoint

--- app.py
+++ app.py
@@ -11,6 +11,24 @@ from flask_cors import CORS
 # configuration
 DEBUG = True
 
+TASKS = [
+    {
+        'title': 'Scaffold front-end',
+        'owner': 'Joshah',
+        'complete': True
+    },
+    {
+        'title': 'Production JS Server',
+        'owner': 'N/A',
+        'complete': False
+    },
+    {
+        'title': 'Add CORS to Flask server',
+        'owner': 'Joshah',
+        'complete': False
+    },
+]
+
 # instantiate app
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -22,6 +40,13 @@ CORS(app, resources={f'/*': {'origins': '*'}})
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+@app.route('/tasks', methods=['GET'])
+def all_tasks():
+    return jsonify({
+            'status': 'success',
+            'tasks': TASKS
+        })
 
 
 if __name__ == '__main__':
